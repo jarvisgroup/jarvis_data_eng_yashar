4cfb1620e7c5be06fa19117652abeee2a78a0a16
YasharAhmat <ahyaxar0124@gmail.com>
2019-11-29 21:24:15 +0000

    changing

--- linux_sql/sql/queries.sql
+++ linux_sql/sql/queries.sql
@@ -1,15 +1,22 @@
-/* group hosts by CPU number and sort by their mem siz in descending order
+-- Q1 group hosts by CPU number and sort by their mem siz in descending order
 SELECT cpu_number,host_id,total_mem 
 FROM 
 	PUBLIC.host_info
 ORDER BY
+	cpu_number,
 	totak_mem DESC;
 
 
-
-SELECT host_id,host_name,total_mem,
-	AVG( )      AS used_memory_percentage
+-- Q2 
+SELECT host_usage.host_id AS host_id,
+	host_info.host_name AS host_name,
+	host_info.total_mem AS total_memory,
+	AVG(100*(host_info.total_mem-host.usage.memory_free)/host_info.total_mem)  
+	AS used_memory_percentage
 FROM 
-	PUBLIC.host_info
+	PUBLIC.host_usage
+	INNER JOIN host_info ON host_usage.host_id = host_info.id
+GROUP BY 
+	DATE_TRUNC('hour',host_usage.timestamp) + DATE_PART('minute',host_usage.timestamp)::	
 ORDER BY 
-	used_memory_percentage DESC;
+	host_usage.host_id;
