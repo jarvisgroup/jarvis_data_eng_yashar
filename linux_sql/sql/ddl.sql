4d43f8703e97ab1af054f67f22f5a676bf816891
YasharAhmat <ahyaxar0124@gmail.com>
2019-11-29 17:38:43 +0000

    complete ddl

--- linux_sql/sql/ddl.sql
+++ linux_sql/sql/ddl.sql
@@ -1,31 +1,32 @@
-/*CREATE DATABASE host_agent */
-
+--CREATE DATABASE host_agent
+DROP DATABASE IF EXISTS host_agent;
 CREATE DATABASE host_agent;
 
-/* switch to host_agent db
+--  switch to host_agent db
 \c host_agent;
 
-/* create table to store hardware specifications */
+-- create table to store hardware specifications 
 CREATE TABLE IF NOT EXISTS public.host_info(
-	id SERIAL NOT NULL SERIAL PRIMARY KEY,
-	host_name VARCHAR NOT NULL,
-	cpu_number INTEGER NOT NULL,
+	id SERIAL NOT NULL PRIMARY KEY,
+	host_name VARCHAR NOT NULL UNIQUE,
+	cpu_number INT2 NOT NULL,
 	cpu_architecture VARCHAR NOT NULL,
 	cpu_model VARCHAR NOT NULL,
 	cpu_mhz DECIMAL NOT NULL,
-	L2_cache(kB) VARCHAR NOT NULL,
-	timestamp TIMESTAMP NOT NULL
+	L2_cache VARCHAR NOT NULL,
+	"timestamp" TIMESTAMP NOT NULL,
+	total_mem INTEGER
 );
 
-/* CREATE TABLE to resource usage data */
+--  CREATE TABLE to resource usage data 
 CREATE TABLE IF NOT EXISTS public.host_usage(
-	'timestamp' TIMESTAMP NOT NULL,
+	"timestamp" TIMESTAMP NOT NULL,
 	host_id SERIAL NOT NULL REFERENCES public.host_info(id),
-	memory_free(MB) INTEGER NOT NULL ,
-	cpu_idel(%) INTEGER NOT NULL,
-	cpu_kernel(%) INTEGER NOT NULL,
+	memory_free INTEGER NOT NULL ,
+	cpu_idel INTEGER NOT NULL,
+	cpu_kernel INTEGER NOT NULL,
 	disk_id INTEGER NOT NULL,
-	disk_avaiable(MB) INTEGER NOT NULL
+	disk_avaiable INTEGER NOT NULL
 );
 
 
