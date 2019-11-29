9638e88f4096dba0c263164e34b962ccfe9d4f31
YasharAhmat <ahyaxar0124@gmail.com>
2019-11-29 22:21:57 +0000

    finished second query

--- linux_sql/sql/queries.sql
+++ linux_sql/sql/queries.sql
@@ -11,12 +11,15 @@ ORDER BY
 SELECT host_usage.host_id AS host_id,
 	host_info.host_name AS host_name,
 	host_info.total_mem AS total_memory,
-	AVG(100*(host_info.total_mem-host.usage.memory_free)/host_info.total_mem)  
-	AS used_memory_percentage
+	AVG(100*(host_info.total_mem-host.usage.memory_free)/host_info.total_mem) AS used_memory_percentage
 FROM 
 	PUBLIC.host_usage
 	INNER JOIN host_info ON host_usage.host_id = host_info.id
-GROUP BY 
-	DATE_TRUNC('hour',host_usage.timestamp) + DATE_PART('minute',host_usage.timestamp)::	
+GROUP BY
+	host_id,
+	host_name,
+	total_memory, 
+	DATE_TRUNC('hour',host_usage.timestamp) + 
+	DATE_PART('minute',host_usage.timestamp)::int/5 * interval'5 min' AS time
 ORDER BY 
 	host_usage.host_id;
